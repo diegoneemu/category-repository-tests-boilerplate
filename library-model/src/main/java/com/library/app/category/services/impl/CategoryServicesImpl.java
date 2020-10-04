144442b65b1193b9a0e51fe33812fd86beab53bb
Diego Augusto <diego.augusto@neemu.com>
2020-10-04 17:31:20 -0300

    feat: ensure that throws exception when try update invalid category and update category correctly

--- library-model/src/main/java/com/library/app/category/services/impl/CategoryServicesImpl.java
+++ library-model/src/main/java/com/library/app/category/services/impl/CategoryServicesImpl.java
@@ -7,6 +7,7 @@ import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
 
 import com.library.app.category.exception.CategoryExistentException;
+import com.library.app.category.exception.CategoryNotFoundException;
 import com.library.app.category.model.Category;
 import com.library.app.category.repository.CategoryRepository;
 import com.library.app.category.services.CategoryServices;
@@ -34,6 +35,29 @@ public class CategoryServicesImpl implements CategoryServices {
 		}
 
 		return categoryRepository.add(category);
+	}
+
+	@Override
+	public void update(final Category category) {
+		final Set<ConstraintViolation<Category>> errors = validator.validate(category);
+		final Iterator<ConstraintViolation<Category>> itErrors = errors.iterator();
+
+		if (itErrors.hasNext()) {
+			final ConstraintViolation<Category> violation = itErrors.next();
+			throw new FieldNotValidException(violation.getPropertyPath().toString(), violation.getMessage());
+		}
+
+		final boolean alreadyExists = categoryRepository.alreadyExists(category);
+
+		if (alreadyExists) {
+			throw new CategoryExistentException();
+		}
+
+		if(!categoryRepository.existsById(category.getId())){
+			throw new CategoryNotFoundException();
+		}
+
+		categoryRepository.update(category);
 	}
 
 }
